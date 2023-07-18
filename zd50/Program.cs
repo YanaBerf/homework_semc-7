8bdc9dec52b412c47dcad19cfda347f44861c8e7
[YanaB] <[tudabae@gmail.com]>
2023-07-18 13:33:42 +0300

    найти элемент в двумерном массивве

---
+++ zd50/Program.cs
@@ -0,0 +1,131 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента 
+в двумерном массиве, 
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+//Напишите программу, которая на вход принимает значение элемента в двумерном массиве, 
+//и возвращает информацию о наличии этого элемента в массиве.
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] arr = GetArray(rows, columns);
+PrintArray(arr);
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(FindElement(array, num))
+{
+    Console.WriteLine("Число найдено");
+}
+else
+{
+    Console.WriteLine("Число не найдено");
+}
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(1, 20);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+bool FindElement(int[,] array, int num)
+{
+     for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if(array[i,j] == num) return true;      
+        }
+    }return false;
+}
+
+
+
+
+/*using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows=int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns=int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns,0,10);
+PrintArray(array);
+Write("Введите значение элемента: ");
+int element=int.Parse(ReadLine());
+if(FindElement(array,element))
+{
+    WriteLine("Элемент найден");
+}
+else
+{
+    WriteLine("Элемент не найден");
+}
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max+1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i,j]} ");
+        }
+        WriteLine();
+    }
+}
+
+bool FindElement(int[,] array, int el)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j]==el) return true;
+        }
+    }
+    return false;
+}
+*/
+
