8d2e48855e65a1b1a602671c3170b430af612515
[YanaB] <[tudabae@gmail.com]>
2023-07-18 13:34:34 +0300

    среднеарифметическое столбца в двумерн массиве

---
+++ zd51/Program.cs
@@ -0,0 +1,125 @@
+﻿/*адача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+int[,] arr = GetArray(rows, columns, 0, 9);
+PrintArray(arr);
+double[] array = GetMeanColumns(arr);
+Console.Write($"Среднее арифметическое каждого столбца = {String.Join("; ",array)}");
+double[] GetMeanColumns(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sum=0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum+=array[j,i];
+        }
+        result[i]=Math.Round(sum/array.GetLength(0),2);
+    }
+    return result;
+}
+
+/*double[] GetMeanColumns(int [,] inArray)
+{   
+    double[] result = new double[inArray.GetLength(0)];
+    for (int i = 0; i < inArray.GetLength(0); i++){
+        double sum = 0;
+        for (int j = 0; j< inArray.GetLength(1); j++){
+            sum += inArray[i,j];
+        }   
+            result[i]= Math.Round(sum/inArray.GetLength(1),2);
+    }return result;
+}
+*/
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+/*
+//Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+double[] averageColumns=GetResultArray(array);
+WriteLine($"Среднее арифметическое каждого столбца = {String.Join("; ",averageColumns)}");
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();
+    }
+}
+
+double[] GetResultArray(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        double sum=0;
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            sum+=array[j,i];
+        }
+        result[i]=Math.Round(sum/array.GetLength(0),2);
+    }
+    return result;
+}*/
